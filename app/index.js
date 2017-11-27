d531772e6328bebe6b030b9084bbf609def4fc77
Donghee Lee <jacobdhlee@gmail.com>
2017-11-27 12:38:59 -0800

    currency project open

---
+++ app/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Fuck you! at index.js</Text>
+      </View>
+    );
+  }
+}
