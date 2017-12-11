43ab8c3e0e7ba3328117d5635d715932f1561cf9
Donghee Lee <jacobdhlee@gmail.com>
2017-12-11 11:52:08 -0500

    successfully fetch data

--- app/config/saga.js
+++ app/config/saga.js
@@ -1,13 +1,32 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, select, call, put } from 'redux-saga/effects';
 import {
   SWAP_CURRENCY,
   CHANGE_BASE_CURRENCY,
   GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
 } from '../actions/currencies';
 
+const getRate = currency =>
+  fetch(`https://api.fixer.io/latest?base=${currency}`);
+
 const fetchConversionRate = function* (action) {
-  console.log('TODO: update things', action);
-  yield;
+  try {
+    let currency = action.currency;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    console.log('fucking currency = ', currency);
+    const response = yield call(getRate, currency);
+    const result = yield response.json();
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (e) {
+    yield put({ type: CONVERSION_ERROR, error: e.message });
+  }
 };
 
 const rootSaga = function* () {
